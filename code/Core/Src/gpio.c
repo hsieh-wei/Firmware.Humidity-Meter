82ab32d0489dbdb64b2af22da14a090b7169dc26
hsieh-wei <wade8954@gmail.com>
2025-09-24 12:01:07 +0800

    feat: interrupt

--- code/Core/Src/gpio.c
+++ code/Core/Src/gpio.c
@@ -55,8 +55,8 @@ void MX_GPIO_Init(void)
 
   /*Configure GPIO pins : PE3 PE4 */
   GPIO_InitStruct.Pin = GPIO_PIN_3|GPIO_PIN_4;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PA6 PA7 */
@@ -65,6 +65,13 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI3_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI3_IRQn);
+
+  HAL_NVIC_SetPriority(EXTI4_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI4_IRQn);
 
 }
 
--- code/Core/Src/main.c
+++ code/Core/Src/main.c
@@ -18,8 +18,6 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "stm32f407xx.h"
-#include "stm32f4xx_hal_gpio.h"
 #include "usart.h"
 #include "gpio.h"
 
@@ -99,18 +97,6 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-  
-    // if(Button_Pressed(GPIOE, GPIO_PIN_4) == 1){
-    //   Led_On(GPIOA, GPIO_PIN_6);
-    // }
-    // else{
-    //   Led_Off(GPIOA, GPIO_PIN_6);
-    // }
-
-    // if(Button_Pressed(GPIOE, GPIO_PIN_3) == 1){
-    //   Led_Toggle(GPIOA, GPIO_PIN_7);
-    //   while (Button_Pressed(GPIOE, GPIO_PIN_3) == 1) {}
-    // }
     Led_On(GPIOA, GPIO_PIN_6);
     HAL_Delay(500);
     Led_Off(GPIOA, GPIO_PIN_6);
