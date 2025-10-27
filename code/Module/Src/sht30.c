7c4b154d2f07605ff6734968181864977a1a68c9
hsieh-wei <wade8954@gmail.com>
2025-10-27 11:30:04 +0800

    feat: sht30 module dma in main

--- code/Module/Src/sht30.c
+++ code/Module/Src/sht30.c
@@ -90,11 +90,10 @@ int sht30_measure_data_dma(SHT30_HANDLE *handle)
     }
 
     // Repeatability High, Disabled Clock Stretching Measurement
-    if(handle->status == SHT30_TX_DONE || handle->status == SHT30_RX_DONE){
-        handle->tx_buf[0] = 0x24; handle->tx_buf[1] = 0x00;
-        if (HAL_I2C_Master_Transmit_DMA(handle->hi2c, handle->i2c_address, handle->tx_buf, 2) != HAL_OK) return SHT30_ERROR;
-        handle->status = SHT30_TX_TRANSMITTED;
-    }
+    handle->tx_buf[0] = 0x24; handle->tx_buf[1] = 0x00;
+    if (HAL_I2C_Master_Transmit_DMA(handle->hi2c, handle->i2c_address, handle->tx_buf, 2) != HAL_OK) return SHT30_ERROR;
+    handle->status = SHT30_TX_TRANSMITTED;
+
     return SHT30_SUCCESS;
 }
 
@@ -108,10 +107,9 @@ int sht30_get_data_dma(SHT30_HANDLE *handle)
     HAL_Delay(15); 
 
     // get measurement data
-    if(handle->status == SHT30_TX_DONE){
-        if (HAL_I2C_Master_Receive_DMA(handle->hi2c, handle->i2c_address, handle->rx_buf, 6) != HAL_OK) return SHT30_ERROR;
-        handle->status = SHT30_RX_REQUESTED;
-    }
+    if (HAL_I2C_Master_Receive_DMA(handle->hi2c, handle->i2c_address, handle->rx_buf, 6) != HAL_OK) return SHT30_ERROR;
+    handle->status = SHT30_RX_REQUESTED;
+
     return SHT30_SUCCESS;
 }
 
@@ -121,23 +119,21 @@ int sht30_compute_data(SHT30_HANDLE *handle)
         return SHT30_ERROR;
     }
 
-    if(handle->status == SHT30_RX_DONE){
-        // CRC Validate
-        if (sht30_crc8(&handle->rx_buf[0]) != handle->rx_buf[2]) return SHT30_ERROR;
-        if (sht30_crc8(&handle->rx_buf[3]) != handle->rx_buf[5]) return SHT30_ERROR;
+    // CRC Validate
+    if (sht30_crc8(&handle->rx_buf[0]) != handle->rx_buf[2]) return SHT30_ERROR;
+    if (sht30_crc8(&handle->rx_buf[3]) != handle->rx_buf[5]) return SHT30_ERROR;
 
-        // Calculate Temperature
-        uint16_t raw_temperature = (uint16_t)((handle->rx_buf[0] << 8) | handle->rx_buf[1]);
-        handle->temperature = -45.0f + 175.0f * ((float)raw_temperature / (65535.0f)); // 65535 = 2^16-1
+    // Calculate Temperature
+    uint16_t raw_temperature = (uint16_t)((handle->rx_buf[0] << 8) | handle->rx_buf[1]);
+    handle->temperature = -45.0f + 175.0f * ((float)raw_temperature / (65535.0f)); // 65535 = 2^16-1
 
-        // Calculate Humidity
-        uint16_t raw_humidity = (uint16_t)((handle->rx_buf[3] << 8) | handle->rx_buf[4]);
-        handle->humidity = 100 * (float)raw_humidity / 65535.0f; // 65535 = 2^16-1
+    // Calculate Humidity
+    uint16_t raw_humidity = (uint16_t)((handle->rx_buf[3] << 8) | handle->rx_buf[4]);
+    handle->humidity = 100 * (float)raw_humidity / 65535.0f; // 65535 = 2^16-1
 
-        handle->status = SHT30_COMPUTE_DONE;
-        return SHT30_SUCCESS;
-    }
-    return SHT30_ERROR;
+    handle->status = SHT30_COMPUTE_DONE;
+
+    return SHT30_SUCCESS;
 }
 
 // --------------------------------------------------------------------------
