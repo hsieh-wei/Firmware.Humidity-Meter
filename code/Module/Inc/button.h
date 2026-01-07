4e9b7388ad461221fbe684a98d7976994156f577
hsieh-wei <wade8954@gmail.com>
2026-01-07 11:05:46 +0800

    feat: first task

--- code/Module/Inc/button.h
+++ code/Module/Inc/button.h
@@ -5,20 +5,20 @@
 #include <stdint.h>
 
 // --------------------------------------------------------------------------
-// Error Codes 
+// Error Codes
 // --------------------------------------------------------------------------
 typedef enum {
-    BUTTON_SUCCESS  = 0,
-    BUTTON_ERROR    = -1,
+  BUTTON_SUCCESS = 0,
+  BUTTON_ERROR = -1,
 } BUTTON_ERR_t;
 
 // --------------------------------------------------------------------------
 // Handle
 // --------------------------------------------------------------------------
 typedef struct {
-    GPIO_TypeDef *gpiox;  // gpio HAL handle (EX: &GPIOA) 
-    uint16_t gpio_pin;    // gpio HAL pin (EX: GPIO_PIN_6) 
-    int pressed;          // 1 is pressed, 0 is not pressed
+  GPIO_TypeDef *gpiox; // gpio HAL handle (EX: &GPIOA)
+  uint16_t gpio_pin;   // gpio HAL pin (EX: GPIO_PIN_6)
+  int pressed;         // 1 is pressed, 0 is not pressed
 } BUTTON_HANDLE;
 
 // --------------------------------------------------------------------------
@@ -27,7 +27,6 @@ typedef struct {
 int button_pressed(BUTTON_HANDLE *handle);
 
 // --------------------------------------------------------------------------
-// HAL Weak Callback re define 
+// HAL Weak Callback re define
 // --------------------------------------------------------------------------
-void button_gpio_exti(BUTTON_HANDLE *handle, uint16_t GPIO_Pin);
 #endif
--- code/Module/Src/button.c
+++ code/Module/Src/button.c
@@ -1,27 +1,17 @@
 #include "button.h"
 #include "led.h"
 // --------------------------------------------------------------------------
-// Internal Helpers 
+// Internal Helpers
 // --------------------------------------------------------------------------
 
 // --------------------------------------------------------------------------
 // API
 // --------------------------------------------------------------------------
-int button_pressed(BUTTON_HANDLE *handle)
-{
-    handle->pressed  = 0 ;
-    if(HAL_GPIO_ReadPin(handle->gpiox, handle->gpio_pin) == GPIO_PIN_RESET){ // active-low
-        handle->pressed  = 1 ;// button pressed
-    }; 
-    return BUTTON_SUCCESS;
-}
-
-void button_gpio_exti(BUTTON_HANDLE *handle, uint16_t GPIO_Pin){
-    if(handle && handle->gpio_pin == GPIO_Pin){
-        static LED_HANDLE s_led_handle ={
-            .gpiox = GPIOA,
-            .gpio_pin = GPIO_PIN_6,
-        };
-        led_toggle(&s_led_handle);
-    }
+int button_pressed(BUTTON_HANDLE *handle) {
+  handle->pressed = 0;
+  if (HAL_GPIO_ReadPin(handle->gpiox, handle->gpio_pin) ==
+      GPIO_PIN_RESET) {  // active-low
+    handle->pressed = 1; // button pressed
+  };
+  return BUTTON_SUCCESS;
 }
