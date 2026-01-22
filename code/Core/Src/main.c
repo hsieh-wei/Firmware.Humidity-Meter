a9518e78ed157793783d0e5529f28fd0f7ead9b6
hsieh-wei <wade8954@gmail.com>
2026-01-22 21:14:54 +0800

    feat: sensor task in main

--- code/Core/Src/main.c
+++ code/Core/Src/main.c
@@ -13,8 +13,9 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "board_config.h"
-#include "led_blinking_task.h"
 #include "system_state.h"
+#include "led_blinking_task.h"
+#include "sensor_measure_task.h"
 
 /* USER CODE END Includes */
 
@@ -50,90 +51,77 @@ void MX_FREERTOS_Init(void);
  * @retval int
  */
 int main(void) {
+    /* USER CODE BEGIN 1 */
+    /* USER CODE END 1 */
 
-  /* USER CODE BEGIN 1 */
-  /* USER CODE END 1 */
+    /* MCU Configuration--------------------------------------------------------*/
 
-  /* MCU Configuration--------------------------------------------------------*/
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick.
+     */
+    HAL_Init();
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick.
-   */
-  HAL_Init();
+    /* USER CODE BEGIN Init */
+    /* USER CODE END Init */
 
-  /* USER CODE BEGIN Init */
-  /* USER CODE END Init */
+    /* Configure the system clock */
+    SystemClock_Config();
 
-  /* Configure the system clock */
-  SystemClock_Config();
+    /* USER CODE BEGIN SysInit */
+    /* USER CODE END SysInit */
 
-  /* USER CODE BEGIN SysInit */
-  /* USER CODE END SysInit */
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    MX_DMA_Init();
+    MX_I2C1_Init();
+    MX_SPI1_Init();
+    MX_USART1_UART_Init();
+    MX_TIM6_Init();
+    MX_TIM2_Init();
+    /* USER CODE BEGIN 2 */
+    // --------------------------------------------------------------------------
+    // Initial System State and Create TASK
+    // 1.1 Initial System State
+    // 2.1 Allocate memory from the Heap for Task's stack.
+    // 2.2 Create TCB
+    // 2.3 Add this Task to the Ready List
+    // --------------------------------------------------------------------------
+    system_state_init();
+    static SENSOR_MEASURE_TASK_PARAMETER sht30_task_parameter = {
+        .target_sht30 = &g_sht30_handle,
+    };
+    xTaskCreate(sensor_measure_task,    // function pointer
+                "SHT30_SENSOR_TASK",    // task name using in debug
+                128,                    // stack size (words)
+                &sht30_task_parameter,  // parameter into task function
+                1,                      // task priority
+                NULL);                  // handle using in suspend, delete, notify
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_DMA_Init();
-  MX_I2C1_Init();
-  MX_SPI1_Init();
-  MX_USART1_UART_Init();
-  MX_TIM6_Init();
-  MX_TIM2_Init();
-  /* USER CODE BEGIN 2 */
-  // --------------------------------------------------------------------------
-  // Initial System State and Create TASK
-  // 1.1 Initial System State
-  // 2.1 Allocate memory from the Heap for Task's stack.
-  // 2.2 Create TCB
-  // 2.3 Add this Task to the Ready List
-  // --------------------------------------------------------------------------
-  system_state_init();
+    // --------------------------------------------------------------------------
+    // 1. osKernelInitialize: Initializes the Ready, Blocked, Suspended lists of
+    // FreeRTOS.
+    // 2. MX_FREERTOS_Init:  Created CubeMX Tasks. If using native API ,this is
+    // only has a defaultTask.
+    // 3. osKernelStart: Sets up SysTick(OS Tick Timer), Creates Idle Tasks,
+    // Starts the first Task
+    // --------------------------------------------------------------------------
+    /* USER CODE END 2 */
 
-  static LED_BLINKING_TASK_PARAMETER led_d2_task_parameter = {
-      .target_led = &g_led_handle_d2,
-      .blinking_period = 200,
-  };
-  xTaskCreate(led_blinking_task,      // function pointer
-              "LED_TOGGLE_Task",      // task name using in debug
-              128,                    // stack size (words)
-              &led_d2_task_parameter, // parameter into task function
-              1,                      // task priority
-              NULL);                  // handle using in suspend, delete, notify
+    /* Init scheduler */
+    osKernelInitialize(); /* Call init function for freertos objects (in
+                             cmsis_os2.c) */
+    MX_FREERTOS_Init();
 
-  static LED_BLINKING_TASK_PARAMETER led_d3_task_parameter = {
-      .target_led = &g_led_handle_d3,
-      .blinking_period = 500,
-  };
-  xTaskCreate(led_blinking_task,      // function pointer
-              "LED_TOGGLE_Task",      // task name using in debug
-              128,                    // stack size (words)
-              &led_d3_task_parameter, // parameter into task function
-              1,                      // task priority
-              NULL);                  // handle using in suspend, delete, notify
-  // --------------------------------------------------------------------------
-  // 1. osKernelInitialize: Initializes the Ready, Blocked, Suspended lists of
-  // FreeRTOS.
-  // 2. MX_FREERTOS_Init:  Created CubeMX Tasks. If using native API ,this is
-  // only has a defaultTask.
-  // 3. osKernelStart: Sets up SysTick(OS Tick Timer), Creates Idle Tasks,
-  // Starts the first Task
-  // --------------------------------------------------------------------------
-  /* USER CODE END 2 */
+    /* Start scheduler */
+    osKernelStart();
 
-  /* Init scheduler */
-  osKernelInitialize(); /* Call init function for freertos objects (in
-                           cmsis_os2.c) */
-  MX_FREERTOS_Init();
+    /* We should never get here as control is now taken by the scheduler */
 
-  /* Start scheduler */
-  osKernelStart();
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
+    /* USER CODE END WHILE */
 
-  /* We should never get here as control is now taken by the scheduler */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  /* USER CODE END WHILE */
-
-  /* USER CODE BEGIN 3 */
-  /* USER CODE END 3 */
+    /* USER CODE BEGIN 3 */
+    /* USER CODE END 3 */
 }
 
 /**
@@ -141,41 +129,40 @@ int main(void) {
  * @retval None
  */
 void SystemClock_Config(void) {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
-   */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+    /** Configure the main internal regulator output voltage
+     */
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-   * in the RCC_OscInitTypeDef structure.
-   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 8;
-  RCC_OscInitStruct.PLL.PLLN = 336;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 4;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-    Error_Handler();
-  }
+    /** Initializes the RCC Oscillators according to the specified parameters
+     * in the RCC_OscInitTypeDef structure.
+     */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM = 8;
+    RCC_OscInitStruct.PLL.PLLN = 336;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ = 4;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
 
-  /** Initializes the CPU, AHB and APB buses clocks
-   */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
-                                RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+    /** Initializes the CPU, AHB and APB buses clocks
+     */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK) {
-    Error_Handler();
-  }
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK) {
+        Error_Handler();
+    }
 }
 
 /* USER CODE BEGIN 4 */
@@ -190,13 +177,13 @@ void SystemClock_Config(void) {
  * @retval None
  */
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
-  /* USER CODE BEGIN Callback 0 */
-  /* USER CODE END Callback 0 */
-  if (htim->Instance == TIM7) {
-    HAL_IncTick();
-  }
-  /* USER CODE BEGIN Callback 1 */
-  /* USER CODE END Callback 1 */
+    /* USER CODE BEGIN Callback 0 */
+    /* USER CODE END Callback 0 */
+    if (htim->Instance == TIM7) {
+        HAL_IncTick();
+    }
+    /* USER CODE BEGIN Callback 1 */
+    /* USER CODE END Callback 1 */
 }
 
 /**
@@ -204,8 +191,8 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
  * @retval None
  */
 void Error_Handler(void) {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* USER CODE END Error_Handler_Debug */
+    /* USER CODE BEGIN Error_Handler_Debug */
+    /* USER CODE END Error_Handler_Debug */
 }
 #ifdef USE_FULL_ASSERT
 /**
@@ -216,7 +203,7 @@ void Error_Handler(void) {
  * @retval None
  */
 void assert_failed(uint8_t *file, uint32_t line) {
-  /* USER CODE BEGIN 6 */
-  /* USER CODE END 6 */
+    /* USER CODE BEGIN 6 */
+    /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
