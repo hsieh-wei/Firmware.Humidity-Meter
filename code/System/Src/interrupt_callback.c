e713adb45ac7cdd9be43708159650a55f7343599
hsieh-wei <wade8954@gmail.com>
2026-01-22 10:12:06 +0800

    feat: sht30 module turn to support rtos

--- code/System/Src/interrupt_callback.c
+++ code/System/Src/interrupt_callback.c
@@ -11,5 +11,5 @@ void HAL_I2C_MasterTxCpltCallback(I2C_HandleTypeDef *hi2c) {
 }
 
 void HAL_I2C_MasterRxCpltCallback(I2C_HandleTypeDef *hi2c) {
-  sht30_i2c_master_tx_cplt(&g_sht30_handle, hi2c);
+  sht30_i2c_master_rx_cplt(&g_sht30_handle, hi2c);
 }
