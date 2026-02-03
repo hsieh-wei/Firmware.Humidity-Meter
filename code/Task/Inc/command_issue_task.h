68a48ea8ac37e084bb46946181a0026adb28290b
hsieh-wei <wade8954@gmail.com>
2026-02-03 17:01:53 +0800

    feat: command issue task init

--- code/Task/Inc/command_issue_task.h
+++ code/Task/Inc/command_issue_task.h
@@ -16,3 +16,18 @@ typedef struct {
 void command_issue_task(void *parameter);
 
 #endif  //__COMMAND_ISSUE_TASK_H__
+
+/*
+cmd [0] = item, cmd [1] = sub item, cmd [2] = value
+
+item, sub item
+0,0 = temperature,sht30_temperature_upper_threshold
+0,1 = temperature,sht30_temperature_lower_threshold
+1,0 = humidity,sht30_humidity_upper_threshold;
+1,1 = humidity,sht30_humidity_lower_threshold
+2,0 = lcd,lcd_brightness
+2,1 = lcd,lcd_display_mode
+3,0 = period,sht30_measure_period
+3,0 = period,pc_link_log_report_period
+3,0 = period,lcd_refresh_period
+*/
